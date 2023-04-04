e7cf21d27e2da46bf2142c71da345445cf68cea9
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2023-04-04 14:02:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,32 +5,41 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
 }).addTo(map);
 
-var drawnItems = new L.FeatureGroup();
-map.addLayer(drawnItems);
+var markers = [];
+var polyline;
 
-var drawControl = new L.Control.Draw({
-    edit: {
-        featureGroup: drawnItems
+map.on('click', function(e) {
+    var marker = L.marker(e.latlng).addTo(map);
+    markers.push(marker);
+    
+    if (markers.length > 1) {
+        if (polyline) {
+            map.removeLayer(polyline);
+        }
+        polyline = L.polyline(markers.map(function(m) { return m.getLatLng() }));
+        polyline.addTo(map);
     }
+    
+    updateDistanceAndRotation();
 });
-map.addControl(drawControl);
 
-var distance = 0;
-
-map.on('draw:created', function (e) {
-    var type = e.layerType,
-        layer = e.layer;
-
-    if (type === 'marker') {
-        distance = 0;
-        drawnItems.clearLayers();
-        drawnItems.addLayer(layer);
-    } else if (type === 'polyline') {
-        drawnItems.addLayer(layer);
-        var latlngs = layer.getLatLngs();
-        for (var i = 0; i < latlngs.length - 1; i++) {
-            distance += latlngs[i].distanceTo(latlngs[i + 1]);
+function updateDistanceAndRotation() {
+    var distance = 0;
+    if (markers.length > 1) {
+        for (var i = 0; i < markers.length - 1; i++) {
+            distance += markers[i].getLatLng().distanceTo(markers[i + 1].getLatLng());
         }
     }
     document.getElementById('distance').innerHTML = 'Total Distance: ' + distance.toFixed(2) + ' meters';
-});
+
+    if (markers.length > 1) {
+        var prevLatLng = markers[markers.length - 2].getLatLng();
+        var currLatLng = markers[markers.length - 1].getLatLng();
+        var dx = currLatLng.lat - prevLatLng.lat;
+        var dy = currLatLng.lng - prevLatLng.lng;
+        var angle = Math.atan2(dy, dx) * 180 / Math.PI;
+        document.getElementById('rotation').innerHTML = 'Rotation: ' + angle.toFixed(2) + ' degrees';
+    }
+}
+
+
